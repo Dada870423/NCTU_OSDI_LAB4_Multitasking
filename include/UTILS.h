d2f67ba7e4ceec94fbd7cfe5927cbdbde7928f0e
Dada870423 <dada0423.cs05@g2.nctu.edu.tw>
2020-04-18 07:41:21 -0700

    sched init

---
+++ include/UTILS.h
@@ -0,0 +1,7 @@
+#ifndef	_UTILS_H_
+#define	_UTILS_H_
+
+extern void put32 ( unsigned long, unsigned int );
+extern unsigned int get32 ( unsigned long );
+
+#endif
--- src/util.c
+++ src/util.c
@@ -2,53 +2,55 @@
 #include "../include/uart.h"
 void Print_Int(unsigned long long int input)
 {
-	int Flag = 0, i;
-	char digit;
-	char ar[100];
-	//init array
-	for(i=0;i<100;i++) ar[i] = '0';
-	//put input into array
-	i = 99;
-	while(input > 0 && i > 0)
-	{
-		digit = (input % 10) + '0';
-		input = input / 10;
-		ar[i--] = digit;
-	}
-	//print
-	for(i=0;i<100;i++)
-	{
-		if(Flag == 0)
-		{
-			if(ar[i] == '0') continue;
-			else
-			{
-				Flag = 1;
-				uart_send(ar[i]);
-			}
-		}
-		else uart_send(ar[i]);
-	}
+    int Flag = 0, i;
+    char digit;
+    char ar[100];
+    //init array
+    for(i=0;i<100;i++) ar[i] = '0';
+    //put input into array
+    i = 99;
+    while(input > 0 && i > 0)
+    {
+        digit = (input % 10) + '0';
+        input = input / 10;
+        ar[i--] = digit;
+    }
+    //print
+    for(i=0;i<100;i++)
+    {
+        if(Flag == 0)
+        {
+            if(ar[i] == '0') continue;
+            else
+            {
+                Flag = 1;
+                uart_send(ar[i]);
+            }
+        }
+        else uart_send(ar[i]);
+    }
 }
 
 int StrCmp(char *input, char* command, int input_length, int command_length)
 {
-	int i, Equal = 1;
-	if(input_length != command_length) return 0;
-	for(i=0;i<input_length;i++)
-	{
-		if(input[i] != command[i]) Equal = 0;
-	}
-	return Equal;
+    int i, Equal = 1;
+    if(input_length != command_length) return 0;
+    for(i=0;i<input_length;i++)
+    {
+        if(input[i] != command[i]) Equal = 0;
+    }
+    return Equal;
 }
 
-void reverse(char str[], int length) {
+void reverse(char str[], int length) 
+{
     int start = 0;
     int end = length -1;
-    while (start < end) {
-		char tmp  = *(str+start);
-		*(str+start) = *(str+end);
-		*(str+end) = tmp;
+    while (start < end) 
+    {
+        char tmp  = *(str+start);
+        *(str+start) = *(str+end);
+        *(str+end) = tmp;
         //swap(*(str+start), *(str+end));
         start++;
         end--;
@@ -59,17 +61,20 @@ char* itoa(unsigned long long int num, char* str, unsigned long long int base) {
     unsigned long long int i = 0;
     _Bool isNegative = 0;
 
-    if (num == 0) {
+    if (num == 0) 
+    {
         str[i] = '0';
-		i = i + 1;
+        i = i + 1;
         str[i] = '\0';
         return str;
     }
-    if (num < 0 && base == 10) {
+    if (num < 0 && base == 10) 
+    {
         isNegative = 1;
         num = -num;
     }
-    while (num != 0) {
+    while (num != 0) 
+    {
         unsigned long long int rem = num % base;
         str[i++] = (rem > 9)? (rem-10) + 'a' : rem + '0';
         num = num/base;
